f62491ef5d37bb3dab47333dc8e138d69753ba94
Tobias Fried <friedtm@gmail.com>
2019-11-01 19:16:41 -0400

    Made tabs uncloseable

--- src/main/kotlin/com/tobiasfried/iconpacktools/view/MainView.kt
+++ src/main/kotlin/com/tobiasfried/iconpacktools/view/MainView.kt
@@ -1,16 +1,21 @@
 package com.tobiasfried.iconpacktools.view
 
+import javafx.scene.control.TabPane
 import javafx.scene.image.Image
+import javafx.scene.layout.Priority
 import tornadofx.*
 
 class MainView : View("Icon Pack Tools") {
 
     init {
-        setStageIcon(Image("ipt.png"))
+        setStageIcon(Image("bolt.png"))
     }
 
     override val root = tabpane {
         prefWidth = 1000.0
+        tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
+//        tabMinWidth = 108.0
+        hgrow = Priority.ALWAYS
         tab<DrawableView>()
         tab<FilterView>()
         tab("Resource Files")
