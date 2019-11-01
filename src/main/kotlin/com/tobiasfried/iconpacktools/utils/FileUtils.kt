1b531d5c2c3b7140996aaeb3833509a01c99c55f
Tobias Fried <friedtm@gmail.com>
2019-11-01 19:18:43 -0400

    Extract file and path manipulation classes

--- src/main/kotlin/com/tobiasfried/iconpacktools/utils/FileUtils.kt
+++ src/main/kotlin/com/tobiasfried/iconpacktools/utils/FileUtils.kt
@@ -1,2 +1,30 @@
 package com.tobiasfried.iconpacktools.utils
 
+import javafx.util.StringConverter
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class PathConverter : StringConverter<Path>() {
+    override fun toString(path: Path?): String {
+        path?.let { return it.toString() }
+        return ""
+    }
+
+    override fun fromString(string: String?): Path {
+        string?.let { return Paths.get(string.trim()) }
+        return Paths.get("")
+    }
+}
+
+class FileNameConverter : StringConverter<File>() {
+    override fun toString(file: File?): String {
+        file?.let { return file.name }
+        return ""
+    }
+
+    override fun fromString(string: String?): File {
+        string?.let { return File(string) }
+        return File("")
+    }
+}
