d7f6e7f3153c33f678e48e388b787756059848ca
Tobias Fried <friedtm@gmail.com>
2019-11-08 01:34:54 -0500

    Added categorization support and overwrite protection to drawable tool

--- src/main/kotlin/com/tobiasfried/iconpacktools/controller/DrawableController.kt
+++ src/main/kotlin/com/tobiasfried/iconpacktools/controller/DrawableController.kt
@@ -2,8 +2,10 @@ package com.tobiasfried.iconpacktools.controller
 
 import com.tobiasfried.iconpacktools.controller.DrawableOutput.*
 import org.w3c.dom.Document
+import org.xml.sax.InputSource
 import tornadofx.*
 import java.io.File
+import java.io.StringReader
 import java.nio.file.Path
 import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.OutputKeys
@@ -20,16 +22,26 @@ class DrawableController(val updateProgress: (Double, String?) -> Unit) : Contro
     }
 
     //
-    fun createXML(files: List<File>, type: DrawableOutput, path: Path) {
+    fun createXML(files: List<File>, path: Path, overwrite: Boolean = false, type: DrawableOutput) {
         when (type) {
-            DRAWABLE -> exportXML(createDrawableDocument(files), type, path)
-            ICON_PACK -> exportXML(createIconPackDocument(files), type, path)
+            DRAWABLE -> exportXML(createDrawableDocument(files), path, overwrite, type)
+            ICON_PACK -> exportXML(createIconPackDocument(files), path, overwrite, type)
             BOTH -> {
-                exportXML(createDrawableDocument(files), DRAWABLE, path)
-                exportXML(createIconPackDocument(files), ICON_PACK, path)
+                exportXML(createDrawableDocument(files), path, overwrite, DRAWABLE)
+                exportXML(createIconPackDocument(files), path, overwrite, ICON_PACK)
             }
         }
-        updateProgress(1.0, "COMPLETE")
+    }
+
+    fun createCategorizedXML(folders: Map<File, List<File>>, path: Path, overwrite: Boolean = false, type: DrawableOutput) {
+        when (type) {
+            DRAWABLE -> exportXML(createCategorizedDrawableDocument(folders), path, overwrite, type)
+            ICON_PACK -> exportXML(createCategorizedIconPackDocument(folders), path, overwrite, type)
+            BOTH -> {
+                exportXML(createCategorizedDrawableDocument(folders), path, overwrite, DRAWABLE)
+                exportXML(createCategorizedIconPackDocument(folders), path, overwrite, ICON_PACK)
+            }
+        }
     }
 
     private fun createDrawableDocument(files: List<File>): Document {
@@ -44,6 +56,36 @@ class DrawableController(val updateProgress: (Double, String?) -> Unit) : Contro
             resources.appendChild(item)
 
             updateProgress(i / files.size.toDouble(), "$i / ${files.size}")
+        }
+
+        doc.appendChild(resources)
+        return doc
+    }
+
+    private fun createCategorizedDrawableDocument(folders: Map<File, List<File>>): Document {
+        val count = folders.values.flatten().size
+
+        val doc = dBuilder.newDocument()
+        val resources = doc.createElement("resources")
+
+        folders.entries.forEach { entry ->
+            val category = doc.createElement("category").also { it.setAttribute("title", entry.key.name) }
+            resources.appendChild(category)
+            entry.value.forEach {
+                val item = doc.createElement("item")
+                item.setAttribute("drawable", it.nameWithoutExtension)
+                resources.appendChild(item)
+            }
+        }
+
+        val all = doc.createElement("category").also { it.setAttribute("title", "All") }
+        resources.appendChild(all)
+        val flat = folders.values.flatten()
+        for (i in 0..flat.lastIndex){
+            val item = doc.createElement("item")
+            item.setAttribute("drawable", flat[i].nameWithoutExtension)
+            resources.appendChild(item)
+            updateProgress(i / count.toDouble(), "$i / $count")
         }
 
         doc.appendChild(resources)
@@ -88,10 +130,75 @@ class DrawableController(val updateProgress: (Double, String?) -> Unit) : Contro
         return doc
     }
 
-    private fun exportXML(document: Document, type: DrawableOutput, path: Path) {
-        val filename = if (type == DRAWABLE) "drawable.xml" else "icon-pack.xml"
-        transformer.transform(DOMSource(document), StreamResult(File(path.toString(), filename)))
+    private fun createCategorizedIconPackDocument(folders: Map<File, List<File>>): Document {
+        val count = folders.values.flatten().size
+
+        val doc = dBuilder.newDocument()
+        val resources = doc.createElement("resources").also {
+            it.setAttribute("xmlns:tools", "http://schemas.android.com/tools")
+            it.setAttribute("tools:ignore", "MissingTranslation")
+        }
+        val filters = doc.createElement("string-array").also { it.setAttribute("name", "icon_filters") }
+        with (resources) {
+            appendChild(doc.createComment(" Filter Categories "))
+            appendChild(doc.createComment(" Make sure the filters names are the same as the other arrays "))
+            appendChild(filters)
+        }
+        val previews = doc.createElement("string-array").also { it.setAttribute("name", "icons_preview") }
+        val all = doc.createElement("string-array").also { it.setAttribute("name", "all") }
+
+        folders.entries.forEach { entry ->
+            var done = 0
+
+            filters.appendChild(doc.createElement("item").also { it.appendChild(doc.createTextNode(entry.key.name)) })
+            val category = doc.createElement("string-array").also { it.setAttribute("name", entry.key.name) }
+            entry.value.forEach {
+                val item = doc.createElement("item")
+                item.appendChild(doc.createTextNode(it.nameWithoutExtension))
+                category.appendChild(item)
+                all.appendChild(item.cloneNode(true))
+                previews.appendChild(item.cloneNode(true))
+
+                done += 1
+                updateProgress(done / count.toDouble(), "$done / $count")
+            }
+
+            resources.appendChild(category)
+        }
+
+        with (resources) {
+            appendChild(doc.createComment(" All Drawables "))
+            appendChild(all)
+            appendChild(doc.createComment(" Drawables to include in Dashboard Preview "))
+            appendChild(previews)
+        }
+
+        doc.appendChild(resources)
+        return doc
+    }
+
+    private fun exportXML(document: Document, path: Path, overwrite: Boolean, type: DrawableOutput) {
 //        transformer.transform(DOMSource(document), StreamResult(System.out))
+        val filename = if (type == DRAWABLE) "drawable.xml" else "icon-pack.xml"
+        if (overwrite || !File(path.toString(), filename).exists()) {
+            transformer.transform(DOMSource(document), StreamResult(File(path.toString(), filename)))
+            updateProgress(1.0, "COMPLETE")
+        } else updateProgress(0.0, "FILE ALREADY EXISTS")
+    }
+
+    fun parseDrawableXML(file: File) {
+        val inputSource = InputSource(StringReader(file.readText()))
+        val doc = dBuilder.parse(inputSource)
+        val resources = doc.childNodes
+
+        val resourceMap = HashMap<String, ArrayList<String>>()
+
+//        val x = DOMParser()
+        println(resources)
+        for (i in 0 until resources.length) {
+            val child = resources.item(i)
+//            println(child.localName)
+        }
     }
 }
 
